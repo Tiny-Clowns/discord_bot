aa2415a77a84e45f7a64b4b7c9ad6258bb39d4c8
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2024-05-02 06:10:00 +0100

    init

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+
+# The discord command prefix is !
+COMMAND_PREFIX = "!"
+
+
+def read_token():
+    try:
+        file=open("token.txt", "r")
+        return file.readline()
+
+    except Exception as ex:
+        print(ex)
+
+
+
+
+
+
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix=COMMAND_PREFIX)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command not found.")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Missing required arguments.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Bad argument provided.")
+    else:
+        await ctx.send("An error occurred.")
+
+
+
+
+bot.run(read_token())
+
