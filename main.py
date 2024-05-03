2ef3a4799133baa8aaa3e5360d184a47b96e2b57
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2024-05-03 13:15:50 +0100

    Added random_teams functionality to seperate the team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 # The discord command prefix is !
 COMMAND_PREFIX = "!"
@@ -14,16 +15,32 @@ def read_token():
         print(ex)
 
 
-
-
-
-
-
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix=COMMAND_PREFIX)
+
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"Pong!\nLatency: {bot.latency}")
+
+
+@bot.command()
+async def random_teams(ctx, team_quantity: int, *names):
+    if (team_quantity < 2):
+        await ctx.send("Please provide a valid team quantity. (Min 2)")
+        return
+    
+    team_player_quantity = len(names)
+
+    if (team_player_quantity < team_quantity):
+        await ctx.send(f"Not enough players to form teams.\nPlayer quantity provided: {team_player_quantity}\nTeam quantity provided: {team_quantity}\n\nYou need to provide {team_quantity-team_player_quantity} more player(s) to proceed.")
+        return
+
+    names = list(names)
+    random.shuffle(names)
+    teams = [names[i::team_quantity] for i in range(team_quantity)]
+
+    for i, team in enumerate(teams, 1):
+        await ctx.send(f"Team {i}: {', '.join(team)}")
 
 
 @bot.event
@@ -35,7 +52,7 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.BadArgument):
         await ctx.send("Bad argument provided.")
     else:
-        await ctx.send("An error occurred.")
+        await ctx.send("An error occurred.\n" + str(error))
 
 
 
