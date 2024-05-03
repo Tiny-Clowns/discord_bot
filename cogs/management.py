75d9f8904ebbe93dbdca14bbadb7c03cba7b0074
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2024-05-03 17:12:26 +0100

    Added clear command

--- cogs/management.py
+++ cogs/management.py
@@ -4,6 +4,17 @@ class Management(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
+    @commands.command()
+    async def clear(self, ctx, quantity: int):
+
+        if ((0<quantity) and (quantity<101)):
+            messages = await ctx.channel.purge(limit=quantity+1)
+
+            await ctx.send(f"Cleared {len(messages)-1} messages", delete_after=3)
+
+        else:
+            await ctx.send(f"Sorry, minimum purge quantity is 1, maximum is 100. You requested {quantity}.")
+
 
 async def setup(bot):
     await bot.add_cog(Management(bot))
