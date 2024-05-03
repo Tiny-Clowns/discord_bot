2d2d529a35b8880200649e82fdccf88d5c68dfc9
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2024-05-03 20:45:21 +0100

    Resolved duplicated function

--- cogs/entertainment.py
+++ cogs/entertainment.py
@@ -26,29 +26,9 @@ class Entertainment(commands.Cog):
             await ctx.send(f"Team {i}: {', '.join(team)}")
 
     @commands.command()
-    async def random_teams(self, ctx, team_quantity: int, *names):
-        if (team_quantity < 2):
-            await ctx.send("Please provide a valid team quantity. (Min 2)")
-            return
-        
-        team_player_quantity = len(names)
-
-        if (team_player_quantity < team_quantity):
-            await ctx.send(f"Not enough players to form teams.\nPlayer quantity provided: {team_player_quantity}\nTeam quantity provided: {team_quantity}\n\nYou need to provide {team_quantity-team_player_quantity} more player(s) to proceed.")
-            return
-
-        names = list(names)
-        random.shuffle(names)
-        teams = [names[i::team_quantity] for i in range(team_quantity)]
-
-        for i, team in enumerate(teams, 1):
-            await ctx.send(f"Team {i}: {', '.join(team)}")
-
-    @commands.command()
     @commands.has_permissions(move_members=True)
     async def split(self, ctx):
         current_voice_channel = ctx.author.voice.channel
-
 
         new_channel_1 = get(ctx.guild.voice_channels, name="Team 1")
         new_channel_2 = get(ctx.guild.voice_channels, name="Team 2")
