d7d8a379a58c58e3c371f46f1581503c1b861b67
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2024-05-04 13:04:37 +0100

    Added permission requirement for clear command

--- cogs/management.py
+++ cogs/management.py
@@ -5,13 +5,14 @@ class Management(commands.Cog):
         self.bot=bot
 
     @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, quantity: int):
         if ((0<quantity) and (quantity<101)):
             messages = await ctx.channel.purge(limit=quantity+1)
-            await ctx.send(f"Cleared {len(messages)-1} messages", delete_after=3)
+            await ctx.send(f"Cleared {len(messages)-1} messages. (This message will be deleted automatically in 3 seconds.)", delete_after=3)
 
         else:
-            await ctx.send(f"Sorry, minimum purge quantity is 1, maximum is 100. You requested {quantity}.")
+            await ctx.send(f"Sorry, {ctx.author.mention} minimum purge quantity is 1, maximum is 100. You requested {quantity}.")
 
 
 async def setup(bot):
